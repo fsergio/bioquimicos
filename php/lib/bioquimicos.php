bea7e7f86b1bb3ccb77d6da10aaa6ae09d21dda8
fsergio <fernandez12sergio@gmail.com>
2018-07-17 23:31:22 -0300

    abm especialidades, nuevo logo grande

--- php/lib/bioquimicos.php
+++ php/lib/bioquimicos.php
@@ -13,23 +13,19 @@ class bioquimicos {
                 contacto
                 from negocio.obra_social
                 $where order by 3";
+
+        return toba::db()->consultar($sql);
+    }
+
+    function get_especialidades ($where=null) {
+        $where = isset($where) ? "WHERE $where" : '';
+
+        $sql = "  select id, 
+                  descripcion
+                  from negocio.especialidades
+                  $where order by 2";
+
         return toba::db()->consultar($sql);
     }
 }
 ?>
-<!--function get_obras_sociales ($where=null)-->
-<!--{-->
-<!--$where = isset($where) ? " WHERE $where " : '';-->
-<!---->
-<!--$sql = "SELECT 	id,-->
-<!--siglas,-->
-<!--nombre,-->
-<!--direccion,-->
-<!--telefono,-->
-<!--mail,-->
-<!--contacto-->
-<!--FROM negocio.obra_social-->
-<!--$where ORDER BY 3";-->
-<!--return toba::db()->consultar($sql);-->
-<!---->
-<!--}-->
