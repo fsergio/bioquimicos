ef9300a936c42c7a90cd610d3f7cd9fdb2a8d2a0
fsergio <fernandez12sergio@gmail.com>
2018-07-28 20:02:33 -0300

    implementacion ordenes cabecera, update source postgres

--- php/lib/bioquimicos.php
+++ php/lib/bioquimicos.php
@@ -81,12 +81,60 @@ class bioquimicos {
                        m.direccion, 
                        m.telefono, 
                        m.mail, 
-                       m.id_especialidad, 
-                       e.id, 
-                       e.descripcion
-                       FROM negocio.medicos_efectores m 
-                       inner join negocio.especialidades e 
-                       on  m.id_especialidad = e.id
+                       m.id_especialidad
+                       FROM negocio.medicos_efectores m
+                       $where order by 3";
+
+        return toba::db()->consultar($sql);
+    }
+
+    function get_pacientes ($where=null) {
+        $where = isset($where) ? "WHERE $where" : '';
+
+        $sql = "SELECT
+                p.id,
+                p.descripcion,
+                p.id_obra_social,
+                p.numero,
+                o.nombre,
+                o.id
+                FROM negocio.pacientes p
+                inner join negocio.obra_social o
+                on p.id_obra_social = o.id
+                $where order by 3";
+
+        return toba::db()->consultar($sql);
+    }
+
+    function get_orden_cabecera ($where=null) {
+        $where = isset($where) ? "WHERE $where" : '';
+
+        $sql = "SELECT 
+                o.id, 
+                o.fecha_recepcion,
+                o.fecha_prescripcion,
+                o.id_efector, 
+                o.id_bioquimico, 
+                o.id_diagnostico,
+                o.nro_afiliado, 
+                o.id_osocial,
+                m.id as ide,
+                m.nombre_apellido as nomape,
+                b.id as idb,
+                b.nombre_apellido as nomapeb,
+                d.id as idd,
+                d.descripcion as des,
+                s.id as ids,
+                s.nombre as nomobrasocial
+                FROM negocio.orden_cabecera o
+                inner join negocio.medicos_efectores m
+                on o.id_efector = m.id
+                inner join negocio.bioquimicos b
+                on o.id_bioquimico = b.id 
+                inner join negocio.diagnosticos d 
+                on o.id_diagnostico = d.id
+                inner join negocio.obra_social s 
+                on o.id_osocial = s.id
                 $where order by 3";
 
         return toba::db()->consultar($sql);
